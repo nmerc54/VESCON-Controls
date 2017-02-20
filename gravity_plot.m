8731c3bbe810d948bcfdff5837c11c981be4ea23
slu-ssrl <slu.ssrl@gmail.com>
2017-02-20 15:38:02 -0600

    Fized Endless loop problem
    There was an issue with adding to a vector that I was looping over...

--- gravity_plot.m
+++ gravity_plot.m
@@ -4,6 +4,7 @@
 % This is a filler for the B field.
 
 clear all;
+disp('Starting!');
 %% Parameters:
 
 radius_earth = 6371; % km
@@ -20,25 +21,33 @@ max_alt = 10000;
 %mu = 3.986e5;
 mu = 1;
 
-x = 0:1:10;
-y = 0:1:10;
-z = 0:1:10;
-
+disp('Settng up x, y, and z');
+x = -10:1:10;
+y = -10:1:10;
+z = -10:1:10;
+disp('finished creating x, y, and z. Starting meshgrid')
 [X, Y, Z] = meshgrid(x, y, z);
-
+disp('Finished meshgrid. Starting for loop.')
 for i = 1:numel(x)
     for j = 1:numel(y)
         for k = 1:numel(z)
             
-            z(i,j,k) = -mu*(x(i)^2 + y(j)^2 + z(k)^2);
+            fprintf('Im in iteration: i = %d, j = %d, k = %d \n', i, j, k);
+            T(i,j,k) = -mu*(x(i)^2 + y(j)^2 + z(k)^2);
+            
+%             u(i,j,k) = -mu*x(i);
+%             v(i,j,k) = -mu*y(j);
+%             w(i,j,k) = -mu*z(k);
+            
             
         end
     end
 end
 
-
-[dx, dy, dz] = gradient(z);
-quiver3(X, Y, Z, dx, dy, dz, 0);
+disp('finished for loop. about to start gradient');
+[dx, dy, dz] = gradient(T);
+disp('Im finished with the gradient');
+quiver3(X, Y, Z, dx, dy, dz, 2);
 
 
 
