3ef9bc36ed859685f58181bbbc3366dac248ace3
nmerc54 <n.mercadante@yahoo.com>
2017-02-20 21:14:01 -0600

    Fixed some odd changes
    I had to merge a branch and it screwed with the formatting. Got it fixed

--- gravity_plot.m
+++ gravity_plot.m
@@ -4,12 +4,9 @@
 % This is a filler for the B field.
 
 clear all;
-<<<<<<< HEAD
-=======
-debug = false;
+debug = true;
 debug_print(debug, 'Starting!');
 
->>>>>>> b9a8cb2c4e5219d6e9e84baacf68dc23fa431cb1
 %% Parameters:
 
 radius_earth = 6371; % km
@@ -26,52 +23,38 @@ max_alt = 10000;
 %mu = 3.986e5;
 mu = 1;
 
-<<<<<<< HEAD
 x = 0:1:10;
 y = 0:1:10;
 z = 0:1:10;
 
-[X, Y, Z] = meshgrid(x, y, z);
-
-=======
 debug_print(debug, 'Settng up x, y, and z');
 x = -10:1:10;
 y = -10:1:10;
 z = -10:1:10;
+
 debug_print(debug, 'finished creating x, y, and z. Starting meshgrid')
 [X, Y, Z] = meshgrid(x, y, z);
+
 debug_print(debug, 'Finished meshgrid. Starting for loop.')
->>>>>>> b9a8cb2c4e5219d6e9e84baacf68dc23fa431cb1
 for i = 1:numel(x)
     for j = 1:numel(y)
         for k = 1:numel(z)
-            
-<<<<<<< HEAD
-            z(i,j,k) = -mu*(x(i)^2 + y(j)^2 + z(k)^2);
-=======
+
             debug_print(debug, 'Im in iteration: i = ');
             T(i,j,k) = -mu*(x(i)^2 + y(j)^2 + z(k)^2);
             
 %             u(i,j,k) = -mu*x(i);
 %             v(i,j,k) = -mu*y(j);
 %             w(i,j,k) = -mu*z(k);
-            
->>>>>>> b9a8cb2c4e5219d6e9e84baacf68dc23fa431cb1
-            
+                        
         end
     end
 end
 
-<<<<<<< HEAD
-
-[dx, dy, dz] = gradient(z);
-quiver3(X, Y, Z, dx, dy, dz, 0);
-=======
 debug_print(debug, 'finished for loop. about to start gradient');
 [dx, dy, dz] = gradient(T);
 debug_print(debug, 'Im finished with the gradient');
 quiver3(X, Y, Z, dx, dy, dz, 2);
->>>>>>> b9a8cb2c4e5219d6e9e84baacf68dc23fa431cb1
 
 
 
