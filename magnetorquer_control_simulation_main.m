bac0787cc72a87f2eba726b010c29acb9c112ae6
slu-ssrl <slu.ssrl@gmail.com>
2017-02-27 18:11:07 -0600

    Simple updates
    trying to fix Euler.

--- magnetorquer_control_simulation_main.m
+++ magnetorquer_control_simulation_main.m
@@ -24,6 +24,8 @@ screen_size = get(0,'screensize');
 
 print_intro_to_term('Magnetorquer Control Simulation');
 
+d2r = pi/180;
+r2d = 180/pi;
 %% Parameters
 
 mass = 10;              % kg
@@ -40,8 +42,8 @@ I_SC = inertiaTensor(mass, x_dimension, y_dimension, z_dimension);
 I_B = I_SC;
 
 % Orbit Period
-orbitPeriod = 60*90;    % sec
-dt = 0.1;               % sec
+orbitPeriod = 60;    % sec
+dt = 0.001;               % sec
 t = 0 : dt : orbitPeriod;
 
 % Matrix initialization:
@@ -56,9 +58,16 @@ E_dot = zeros(3, numel(t)); % Euler-Rates (inertial-fixed)
 w(:, 1) = [0; 0; 0];    % rad/s
 H(:, 1) = I_B * w(:,1); 
 
-T(1, 25000:30000) = 0.01;       % N-m
-T(2, 30000:35000) = 0.01;       % N-m
-T(3, 35000:40000) = 0.01;       % N-m
+T(1, :) = pulse(dt, 10, 1, 0.001, T(1, :));  % N-m
+T(2, :) = pulse(dt, 11, 1, 0.001, T(2, :));  % N-m
+T(3, :) = pulse(dt, 12, 1, 0.001, T(3, :));  % N-m
+
+T(1, :) = pulse(dt, 30, 1, -0.001, T(1, :));  % N-m
+T(2, :) = pulse(dt, 31, 1, -0.001, T(2, :));  % N-m
+T(3, :) = pulse(dt, 32, 1, -0.001, T(3, :));  % N-m
+
+% % Debug:
+% plot(t./60, T), axis([0 60 -0.01, 0.01]);
 
 %% Simulation
 for i = 2:numel(t)
@@ -67,7 +76,7 @@ for i = 2:numel(t)
     w(:, i) = I_B \ H(:, i);
     
     E_dot(:, i) = (1/sin(0.001 + (Euler(2, i-1)))) .* euler_transform(Euler(:,i-1)) * w(:, i);
-    Euler(:, i) = Euler(:, i) + E_dot(:, i) .* dt;
+    Euler(:, i) = Euler(:, i-1) + E_dot(:, i) .* dt;
 
 end
 
@@ -75,24 +84,24 @@ end
 disp(I_SC);
 
 figure('position',[screen_size(3)/4, screen_size(4)/4, 1000, 500])
-    plot(t, w(1,:), t, w(2,:), t, w(3,:)), grid on,
-    legend({'\omega_{x}', '\omega_{y}', '\omega_{z}'}),
-    title('Body-Rates');
+    subplot(2, 1, 1)
+        plot(t, w(1,:).*r2d, t, w(2,:).*r2d, t, w(3,:).*r2d), grid on,
+        legend({'\omega_{x}', '\omega_{y}', '\omega_{z}'}),
+        title('Body-Rates');
+    subplot(2,1,2)
+        plot(t, T(1,:), t, T(2,:), t, T(3,:)), grid on,
+        legend({'x', 'y', 'z'}),
+        title('Torques');
     
 figure('position',[screen_size(3)/4, screen_size(4)/4, 1000, 500])
-    plot(t, Euler(1,:), t, Euler(2,:), t, Euler(3,:)), grid on,
-    legend({'\Psi', '\Theta', '\Phi'}),
-    title('Euler Angels');    
-
-figure('position',[screen_size(3)/4, screen_size(4)/4, 1000, 500])
-    plot(t, E_dot(1,:), t, E_dot(2,:), t, E_dot(3,:)), grid on,
-    legend({'\Psi_dor', '\Theta_dot', '\Phi_dot'}),
-    title('Euler Rates');
-
-figure('position',[screen_size(3)/4, screen_size(4)/4, 1000, 500])
-    plot(t, T(1,:), t, T(2,:), t, T(3,:)), grid on,
-    legend({'x', 'y', 'z'}),
-    title('Torques');
+    subplot(2, 1, 1)    
+        plot(t, Euler(1,:).*r2d, t, Euler(2,:).*r2d, t, Euler(3,:).*r2d), grid on,
+        legend({'\Psi', '\Theta', '\Phi'}),
+        title('Euler Angels');    
+    subplot(2,1,2)
+        plot(t, E_dot(1,:).*r2d, t, E_dot(2,:).*r2d, t, E_dot(3,:).*r2d), grid on,
+        legend({'\Psi - dot', '\Theta - dot', '\Phi - dot'}),
+        title('Euler Rates');
 
 
 
