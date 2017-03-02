dacefbc9851d4fc1e1baaf4c45a4889f388a171f
nmerc54 <n.mercadante@yahoo.com>
2017-03-02 17:23:09 -0600

    Euler angles working
    I switched over to the quaternion representation and it works.

---
+++ quat2Euler.m
@@ -0,0 +1,35 @@
+%% *********************************************************************** 
+%  Quaternion to Euler Angles
+%  Space Systems Research Laboaratory
+%  --------------------------------
+%  Nicholas Mercadante
+%
+%  Project: Rascal-1
+%
+%
+% Name          | Date      | Description
+% ------------------------------------------------------------------------
+% N.Mercadante   03-02-2017  
+% ------------------------------------------------------------------------
+%
+% COPYRIGHT 2017 Space Systems Research Laboratory, all rights reserved.
+%
+% ************************************************************************
+
+function euler_angles = quat2Euler( quaternion_vector )
+
+    q1 = quaternion_vector(1);
+    q2 = quaternion_vector(2);
+    q3 = quaternion_vector(3);
+    q4 = quaternion_vector(4);
+
+
+    euler_angles = [ ...
+        
+        atan2( 2*(q1*q4 + q2*q3) , 1 - 2*(q1^2 + q2^2) ) ;
+        asin( 2*(q2*q4 - q1*q3) )  ;
+        atan2( 2*(q3*q4 + q1*q2) , 1 - 2*(q2^2 + q3^2) ) ];
+
+
+
+end
