c61904d5e7cb0e025351504904f41650f1df87ff
nmerc54 <n.mercadante@yahoo.com>
2017-03-04 13:09:28 -0600

    Same commit as before
    This script didn't update last commit for some reason...

--- magnetorquer_control_simulation_main.m
+++ magnetorquer_control_simulation_main.m
@@ -12,6 +12,14 @@
 % N.Mercadante   02-26-2017  This script will develop the control
 %                            simulation for the Rascal-1 magnetorquer
 %                            payload.
+%
+%                            Note that there are three refrence frames used
+%                            in this simulation. There is the intertial
+%                            reference frame, which is an earth-fixed
+%                            frame. This frame will be used for the
+%                            environmental torques. There is the space
+%                            reference frame, wich is fixed on the orbit
+%                            path, and then the body-fixed frame.
 % ------------------------------------------------------------------------
 %
 % COPYRIGHT 2017 Space Systems Research Laboratory, all rights reserved.
@@ -30,89 +38,90 @@ r2d = 180/pi;
 
 mass = 10;              % kg
 
-% SPACECRAFT Dimensions -- on SPACECRAFT Axis system
+% SPACECRAFT Dimensions -- on Body Axis system
 x_dimension = 0.100;    % m
 y_dimension = 0.100;    % m
 z_dimension = 0.681;    % m
 
-% Inertia Tensor -- on SPACECRAFT Axis system
-I_SC = inertiaTensor(mass, x_dimension, y_dimension, z_dimension);
-
-% CHANGE!!!!!!!!!!
-I_B = I_SC;
+% Inertia Tensor -- on BODY Axis system
+I_B = inertiaTensor(mass, x_dimension, y_dimension, z_dimension);
 
 % Orbit Period
-orbitPeriod = 60*90;    % sec
-dt = 1;               % sec
+orbitPeriod = 60*1;    % sec
+dt = 0.001;             % sec
 t = 0 : dt : orbitPeriod;
 
 % Matrix initialization:
 
-H = zeros(3, numel(t));     % Angular Momentum (body-coords)
-w = zeros(3, numel(t));     % Body-Fixed rotation rates
-T = zeros(3, numel(t));     % Torque (body-fixed)
-Euler = zeros(3, numel(t)); % Euler-Angles (inertial-fixed)
-q    = zeros(4, numel(t));
-qdot = zeros(4, numel(t));
+H_B = zeros(3, numel(t));     % Angular Momentum (body-coords)
+w_B = zeros(3, numel(t));     % Body-Fixed rotation rates
+T_B = zeros(3, numel(t));     % Torque (body-fixed)
+B_I = zeros(3, numel(t));    
+B_B = zeros(3, numel(t));
 
-% E_dot = zeros(3, numel(t)); % Euler-Rates (inertial-fixed)
+Euler     = zeros(3, numel(t)); % Euler-Angles (inertial-fixed)
+q_BtoI    = zeros(4, numel(t));
+qdot_BtoI = zeros(4, numel(t));
 
 % Initial Values:
-w(:, 1) = [0; 0; 0];    % rad/s
-H(:, 1) = I_B * w(:,1); 
-q(:, 1) = [0; 0; 0; 1]; % In initial position.
+w_B(:, 1) = [0.00; 0.00; 0.00];    % rad/s
+H_B(:, 1) = I_B * w_B(:,1); 
 
-%% Environmental Torques:
-%  ----------------------
+q_BtoI(:, 1) = dc2q( [ 0 0 -1 ;    % In initial position.
+                      -1 0  0 ;
+                       0 1  0 ]); 
 
-% Earth's magnetic field (IGRF Model)
-% INERTIAL REF FRAME
+%% Environmental Effects:
+
+% Earth's magnetic field (IGRF Model) - SPACE FRAME
 c = 0.0005;
-B(1, :) = 0.0005 * sin(c.*t);
-B(2, :) = 0.0005 * cos(c.*t);
-B(3, :) = 0.0005 * sin(c.*t).^2;
+B_I(1, :) = 0.005 * c.*t; %sin(c.*t);
+B_I(2, :) = 0.005 * c.*t; %cos(c.*t);
+B_I(3, :) = 0.005 * c.*t; %sin(c.*t).^2;
 
-% Control torque
-% BODY-FIXED FRAME
-mu = zeros(3, numel(t));
-mu(1, :) = 0.001;
-T(:, 1) = magneticTorque(B(:, 1), mu(:, 1));
+% Initialize the body-fixed B to the space-fized B.
+B_B(:, 1) = B_I(:, 1);
 
-% T(1, :) = pulse(dt, 10, 1, 0.001, T(1, :));  % N-m
-% T(2, :) = pulse(dt, 11, 1, 0.001, T(2, :));  % N-m
-% T(3, :) = pulse(dt, 12, 1, 0.001, T(3, :));  % N-m
-
-plot(t, B, t, mu(1,:));
+% Control torque - BODY FRAME
+mu_B = zeros(3, numel(t));
+mux = 0.1;   mu_B(1,:) = pulse(dt, 15, 1, mux, mu_B(1,:));
+muy = 0*0.1;   mu_B(2,:) = pulse(dt, 25, 1, muy, mu_B(2,:));
+muz = 0*0.1;   mu_B(3,:) = pulse(dt, 30, 1, muz, mu_B(3,:));
 
 %% Simulation
 for i = 2:numel(t)
+    % Shift the B vector from Inertial to Body reference
+    B_B(:, i) = qRotate(q_BtoI(:, i-1), B_I(:, i));
     
-    H(:, i) = H(:, i-1) + T(:, i) .* dt;
-    w(:, i) = I_B \ H(:, i);
+    % Calculate Control Torque in body-frame
+    T_B(:, i) = magneticTorque(B_B(:, i), mu_B(:, i));
+    
+    % Calculate the body rate changes
+    H_B(:, i) = H_B(:, i-1) + T_B(:, i) .* dt;
+    w_B(:, i) = I_B \ H_B(:, i);
     
     % Quaternion Math
-    S = quatSkew(w(:, i));
-    qdot(:, i) = 0.5 .* S * q(:, i-1);
+    S = quatSkew(w_B(:, i));
+    qdot_BtoI(:, i) = 0.5 .* S * q_BtoI(:, i-1);
     
     % Numerically Integrate and Normalize
-    q(:, i) = normalize(  q(:, i-1) + qdot(:, i)*dt  );
+    q_BtoI(:, i) = normalize(  q_BtoI(:, i-1) + qdot_BtoI(:, i)*dt  );
         
-    Euler(:, i) = quat2Euler(q(:, i));
+    Euler(:, i) = quat2Euler(q_BtoI(:, i));
     
 
 end
 
 %% Display Results
-disp(I_SC);
 
 figure('position',[screen_size(3)/4, screen_size(4)/4, 1000, 500])
     subplot(2, 1, 1)
-        plot(t, w(1,:).*r2d, t, w(2,:).*r2d, t, w(3,:).*r2d), grid on,
+        plot(t, w_B(1,:).*r2d, t, w_B(2,:).*r2d, t, w_B(3,:).*r2d), grid on,
         legend({'\omega_{x}', '\omega_{y}', '\omega_{z}'}),
         title('Body-Rates'), xlabel('Seconds'), ylabel('degrees/sec');
     subplot(2,1,2)
-        plot(t, T(1,:), t, T(2,:), t, T(3,:)), grid on,
-        legend({'x', 'y', 'z'}), axis([0 orbitPeriod -0.01 0.01]),
+        plot(t, T_B(1,:), t, T_B(2,:), t, T_B(3,:)), grid on,
+        legend({'x', 'y', 'z'}), %axis([0 orbitPeriod -0.01 0.01]),
         title('Torques');
     
 
@@ -122,8 +131,8 @@ figure('position',[screen_size(3)/4, screen_size(4)/4, 1000, 500])
         legend({'\Psi', '\Theta', '\Phi'}),
         title('Euler Angels'), xlabel('Seconds'), ylabel('degrees');    
     subplot(2,1,2)
-        plot(t, T(1,:), t, T(2,:), t, T(3,:)), grid on,
-        legend({'x', 'y', 'z'}), axis([0 orbitPeriod -0.01 0.01]),
+        plot(t, T_B(1,:), t, T_B(2,:), t, T_B(3,:)), grid on,
+        legend({'x', 'y', 'z'}), %axis([0 orbitPeriod -0.01 0.01]),
         title('Torques');
 
 
